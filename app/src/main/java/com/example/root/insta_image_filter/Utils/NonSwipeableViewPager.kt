b45dd1c801fcf562e33ab333bc262bd164568e1a
Sajjad Yousefnia <sdyussuf@gmail.com>
2018-09-04 18:38:56 +0430

    2

--- app/src/main/java/com/example/root/insta_image_filter/Utils/NonSwipeableViewPager.kt
+++ app/src/main/java/com/example/root/insta_image_filter/Utils/NonSwipeableViewPager.kt
@@ -2,13 +2,31 @@ package com.example.root.insta_image_filter.Utils
 
 import android.content.Context
 import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+import android.widget.Scroller
 
 class NonSwipeableViewPager : ViewPager {
     constructor(context: Context) : super(context) {
-    setMyScroller()
+        setMyScroller()
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        setMyScroller()
     }
 
     private fun setMyScroller() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        try {
+            val viewPager = ViewPager::class.java
+            val scroller = viewPager.getDeclaredField("mScroller")
+            scroller.isAccessible = true
+            scroller.set(this, MyScroller(context))
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun MyScroller(context: Context): Scroller(context,) {
+
     }
 }
