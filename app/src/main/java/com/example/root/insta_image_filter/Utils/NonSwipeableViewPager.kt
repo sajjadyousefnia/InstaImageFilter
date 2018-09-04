1d6f7af3ffd7aee6381460e20f404b581184c741
Sajjad Yousefnia <sdyussuf@gmail.com>
2018-09-04 18:25:30 +0430

    first commit

---
+++ app/src/main/java/com/example/root/insta_image_filter/Utils/NonSwipeableViewPager.kt
@@ -0,0 +1,14 @@
+package com.example.root.insta_image_filter.Utils
+
+import android.content.Context
+import android.support.v4.view.ViewPager
+
+class NonSwipeableViewPager : ViewPager {
+    constructor(context: Context) : super(context) {
+    setMyScroller()
+    }
+
+    private fun setMyScroller() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
